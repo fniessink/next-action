53526e26d451c3d2798afc76bad0ee273d5e1309
Frank Niessink <frank@niessink.com>
2018-05-09 23:27:51 +0200

    Show friendly error message when file cannot be found. Fix for #2.

--- next_action/cli.py
+++ next_action/cli.py
@@ -1,3 +1,5 @@
+import sys
+
 from next_action.todotxt import Task
 from next_action.pick_action import next_action_based_on_priority
 from next_action.arguments import parse_arguments
@@ -5,10 +7,12 @@ from next_action.arguments import parse_arguments
 
 def next_action() -> None:
     filename: str = parse_arguments().file
-    with open(filename, "r") as todotxt_file:
+    try:
+        todotxt_file = open(filename, "r")
+    except FileNotFoundError:
+        print("Can't find {0}".format(filename))
+        return
+    with todotxt_file:
         tasks = [Task(line.strip()) for line in todotxt_file.readlines()]
     action = next_action_based_on_priority(tasks)
-    if action:
-        print(action.text)
-    else:
-        print("Nothing to do!")
+    print(action.text if action else "Nothing to do!")
