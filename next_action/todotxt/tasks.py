2cfd6d02cacf877b19d4e4d8a6370700fcf3a988
Frank Niessink <frank@niessink.com>
2018-06-13 00:13:26 +0200

    Add methods to get the contexts and projects in a collection of tasks.

--- next_action/todotxt/tasks.py
+++ next_action/todotxt/tasks.py
@@ -1,10 +1,17 @@
 """ Collection of Todo.txt tasks. """
 
-from typing import List
+from typing import List, Set
 
 from .task import Task
 
 
 class Tasks(List[Task]):
     """ Collection of Todo.txt tasks. """
-    pass
+
+    def contexts(self) -> Set[str]:
+        """ Return the contexts used in the collection of tasks. """
+        return set().union(*[task.contexts() for task in self])
+
+    def projects(self) -> Set[str]:
+        """ Return the projects used in the collection of tasks. """
+        return set().union(*[task.projects() for task in self])
