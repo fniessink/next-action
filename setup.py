1fdcd11055f935e258c337df348af1011d860cc9
Frank Niessink <frank@niessink.com>
2018-09-06 17:14:12 +0200

    Add Python 3.7 to list of programming languages.

--- setup.py
+++ setup.py
@@ -34,5 +34,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Office/Business :: Scheduling"],
     keywords=["todo.txt", "task management", "todolist"])
