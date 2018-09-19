46dd2e31c675aa9eedbc5561e4054b39401b8b6d
Frank Niessink <frank@niessink.com>
2018-09-19 18:38:49 +0200

    Replace hardcoded path by which command.

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -1,5 +1,6 @@
 """Code to run before and after certain events during testing."""
 
+import shutil
 import subprocess  # nosec
 
 
@@ -17,7 +18,8 @@ def before_all(context):
 def before_scenario(context, scenario):  # pylint: disable=unused-argument
     """Set up arguments."""
     context.files = []
-    context.arguments = ["coverage", "run", "--omit", ".venv/*", "--branch", "--parallel-mode", ".venv/bin/next-action",
+    next_action = shutil.which("next-action")
+    context.arguments = ["coverage", "run", "--omit", ".venv/*", "--branch", "--parallel-mode", next_action,
                          "--config-file"]
 
 
