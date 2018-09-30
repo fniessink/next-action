f059be48d2d8821971971dc44cdfb9b7fb254f59
Frank Niessink <frank@niessink.com>
2018-09-30 21:00:04 +0200

    Sonarqube server (#283)
    * Add local SonarQube sever and scanner services.
    * Get token from Sonarqube server when scanning.

---
+++ utils/sonarqube_token.py
@@ -0,0 +1,11 @@
+"""Get a SonarQube token for sonar-scanner."""
+
+import sys
+
+import requests
+
+
+API_URL = f"{sys.argv[1]}/api/user_tokens/"
+API_ARGS = dict(data=dict(name="admin"), auth=("admin", "admin"))
+requests.post(API_URL + "revoke", **API_ARGS)  # Revoke any previous tokens
+print(requests.post(API_URL + "generate", **API_ARGS).json()["token"])
