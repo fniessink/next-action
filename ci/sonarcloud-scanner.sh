8cd663271a3b5c5bdbd974e79d7deb52ccb8f2a4
Frank Niessink <frank@niessink.com>
2018-10-18 22:08:28 +0200

    Fix variable.

--- ci/sonarcloud-scanner.sh
+++ ci/sonarcloud-scanner.sh
@@ -2,4 +2,4 @@
 
 # SonarQube needs Xunit format, see https://docs.sonarqube.org/display/PLUG/Python+Unit+Tests+Execution+Reports+Import
 nosetests --nocapture --with-xunit --xunit-file=build/nosetests.xml tests/unittests
-sonar-scanner -Dsonar.host.url=http://sonarcloud.io -Dsonar.organization=fniessink-github -Dsonar.login=$$SONAR_TOKEN
+sonar-scanner -Dsonar.host.url=http://sonarcloud.io -Dsonar.organization=fniessink-github -Dsonar.login="$SONAR_TOKEN"
