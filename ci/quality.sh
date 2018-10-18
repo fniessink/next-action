5c5357d2b58e96488ebc55894e80f5dc018e92ea
Frank Niessink <frank@niessink.com>
2018-10-18 17:38:04 +0200

    Move README template to docs folder.

--- ci/quality.sh
+++ ci/quality.sh
@@ -8,7 +8,7 @@ vulture next_action .vulture-whitelist.py
 pyroma --min=10 . 2> /dev/null  # Ignore the error messages on stderr about the README file because pyroma doesn't accept Markdown.
 shellcheck extra/.next-action-completion.bash ci/*.sh
 gherkin-lint tests/features/*.feature
-markdownlint README*.md; markdownlint -c .markdownlint-changelog.json CHANGELOG.md
+markdownlint README.md docs/*.md; markdownlint -c .markdownlint-changelog.json CHANGELOG.md
 hadolint Dockerfile
 docker-compose config --quiet
 
