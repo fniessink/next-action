66931452496ffd6dc418bcf7a82fd52bc1ec0424
Frank Niessink <frank@niessink.com>
2019-11-04 09:28:04 +0100

    Set target at 10 again now that Pyroma accepts Markdown readme files.

--- ci/quality.sh
+++ ci/quality.sh
@@ -5,8 +5,7 @@ pylint next_action tests docs
 pycodestyle .
 pydocstyle .
 vulture next_action .vulture-whitelist.py
-# Set min pyroma to 9 because Pyroma does not yet accept Markdown, see https://github.com/regebro/pyroma/issues/42
-pyroma --min=9 .
+pyroma --min=10 .
 shellcheck extra/.next-action-completion.bash ci/*.sh
 gherkin-lint tests/features/*.feature
 markdownlint README.md docs/*.md; markdownlint -c .markdownlint-changelog.json CHANGELOG.md
